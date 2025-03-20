0ed3c4652a1e8938cf6cb61c477c746e5d636bc2
JEdaurdoPA <A01666860@tec.mx>
2025-03-20 15:28:52 -0600

    La comida y la vibora cambia de color al inicar  la partida

--- juevibora.py
+++ juevibora.py
@@ -1,14 +1,4 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
-from random import randrange
+from random import randrange, choice
 from turtle import *
 
 from freegames import square, vector
@@ -16,7 +6,9 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-
+colors = ['blue', 'yellow', 'green', 'orange', 'gray']
+snake_color = choice(colors)
+food_color = choice([c for c in colors if c != snake_color])
 
 def change(x, y):
     """Change snake direction."""
@@ -51,9 +43,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
-
-    square(food.x, food.y, 9, 'green')
+        square(body.x, body.y, 9, snake_color)
+        square(food.x, food.y, 9, food_color)
+        
     update()
     ontimer(move, 100)
 
